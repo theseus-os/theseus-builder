38b50ece2557ab6417cc5e08c43d88f496921a37
Nathan Royer <nathan.royer.pro@gmail.com>
2022-08-02 11:58:09 +0200

    fix: missing keys in default configuration

--- src/build_cells.rs
+++ src/build_cells.rs
@@ -1,6 +1,7 @@
 use crate::log;
 use crate::oops;
 use crate::opt_str;
+use crate::opt_str_vec;
 use crate::check_result;
 
 use std::process::Command;
@@ -20,7 +21,7 @@ pub fn process(config: &Value) {
     let toolchain = opt_str(config, &["build-cells", "toolchain"]);
 
     let mut cargo_flags = opt_str_vec(config, &["build-cells", "other-cargo-flags"]);
-    cargo_flags.extend_from_slice(&["-Z", "unstable-options"]);
+    cargo_flags.extend_from_slice(&["-Z".into(), "unstable-options".into()]);
 
     let build_std = opt_str_vec(config, &["build-cells", "build-std"]);
     let build_std_flag = format!("build-std={}", build_std.join(","));
@@ -29,8 +30,8 @@ pub fn process(config: &Value) {
     let build_std_features_flag = format!("build-std-features={}", build_std_features.join(","));
 
     if build_std.len() > 0 {
-        cargo_flags.extend_from_slice(&["-Z", &build_std_flag]);
-        cargo_flags.extend_from_slice(&["-Z", &build_std_features_flag]);
+        cargo_flags.extend_from_slice(&["-Z".into(), build_std_flag]);
+        cargo_flags.extend_from_slice(&["-Z".into(), build_std_features_flag]);
     }
 
     let mut rust_flags = opt_str_vec(config, &["build-cells", "other-rust-flags"]);
@@ -39,33 +40,33 @@ pub fn process(config: &Value) {
         // which avoids always having to unpack the crate's .rlib archive to extract the object files within.
         // Note that we still do have to extract and partially link object files from .rlib archives for crates that
         // use a build script to generate additional object files during build time.
-        "--emit=obj",
+        "--emit=obj".into(),
 
         // enable debug info even for release builds
-        "-C debuginfo=2",
+        "-C debuginfo=2".into(),
 
         // using a large code model
-        "-C code-model=large",
+        "-C code-model=large".into(),
 
         // use static relocation model to avoid GOT-based relocation types and .got/.got.plt sections
-        "-C relocation-model=static",
+        "-C relocation-model=static".into(),
 
         // promote unused must-use types (like Result) to an error
-        "-D unused-must-use",
+        "-D unused-must-use".into(),
 
         // As of Dec 31, 2018, this is needed to make loadable mode work, because otherwise, 
         // some core generic function implementations won't exist in the object files.
         // Details here: https://github.com/rust-lang/rust/pull/57268
         // Relevant rusct commit: https://github.com/jethrogb/rust/commit/71990226564e9fe327bc9ea969f9d25e8c6b58ed#diff-8ad3595966bf31a87e30e1c585628363R8
         // Either "trampolines" or "disabled" works here, not sure how they're different
-        "-Z merge-functions=disabled",
+        "-Z merge-functions=disabled".into(),
 
         // This prevents monomorphized instances of generic functions from being shared across crates.
         // It vastly simplifies the procedure of finding missing symbols in the crate loader,
         // because we know that instances of generic functions will not be found in another crate
         // besides the current crate or the crate that defines the function.
         // As far as I can tell, this does not have a significant impact on object code size or performance.
-        "-Z share-generics=no",
+        "-Z share-generics=no".into(),
     ]);
 
     if !["debug", "release"].contains(&build_mode.as_str()) {
@@ -80,7 +81,7 @@ pub fn process(config: &Value) {
             .arg("build")
             .arg("--manifest-path=kernel/Cargo.toml")
             .arg(&format!("--{}", &build_mode))
-            .args(build_std_flags)
+            .args(cargo_flags)
             .args(rust_features)
             .arg("--target")
             .arg(&format!("cfg/{}.json", &target))
